f027743effb11984d17b5489a2e866ca12ca5727
akhilmantha <akhilmantha@live.com>
2018-06-04 17:55:03 +0530

    functions to calculate insets and size has been added

---
+++ FlowLayoutBasics/Starter/Character Collector/CharacterFlowLayout.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterFlowLayout.swift
+//  Character Collector
+//
+//  Created by akhil mantha on 04/06/18.
+//  Copyright © 2018 Razeware, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CharacterFlowLayout: UICollectionViewFlowLayout {
+    let standardItemAlpha : CGFloat = 0.5
+    let standardItemScale : CGFloat = 0.5
+    
+    override func prepare() {
+         super.prepare()
+    }
+}
--- FlowLayoutBasics/Starter/Character Collector/MasterViewController.swift
+++ FlowLayoutBasics/Starter/Character Collector/MasterViewController.swift
@@ -1,27 +1,4 @@
-//
-//  MasterViewController.swift
-//  Character Collector
-/*
- * Copyright (c) 2016 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+
 
 import UIKit
 
@@ -29,7 +6,10 @@ class MasterViewController: UICollectionViewController {
   
   let charactersData = Characters.loadCharacters()
     let columns : CGFloat = 3.0
-      
+    let inset : CGFloat = 8.0
+    let spacing : CGFloat = 8.0
+    let lineSpacing : CGFloat = 8.0
+    var isRandom = false
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -49,6 +29,7 @@ class MasterViewController: UICollectionViewController {
   }
   
   func refreshControlDidFire() {
+    isRandom = true
     collectionView?.reloadData()
     collectionView?.refreshControl?.endRefreshing()
   }
@@ -90,9 +71,28 @@ extension MasterViewController {
 // MARK: UICollectionViewDelegateFlowLayout
 
 extension MasterViewController : UICollectionViewDelegateFlowLayout{
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = Int(collectionView.frame.width/columns)
-        return CGSize(width: width, height: width)
+        let width = Int((collectionView.frame.width/columns) - (inset + spacing))
+        var randomSize : Int
+            if isRandom == true{
+                randomSize = 64 * Int(arc4random_uniform(UInt32(3)) + 1)
+            }
+            else{
+                randomSize = width
+            }
+        return CGSize(width: randomSize, height: randomSize)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetsForSectionAt section: Int) -> UIEdgeInsets{
+        return UIEdgeInsets(top : inset, left : inset, bottom : inset, right : inset)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInterItemSpacingForSectionAt section : Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section : Int) -> CGFloat {
+        return lineSpacing
     }
 }
 
